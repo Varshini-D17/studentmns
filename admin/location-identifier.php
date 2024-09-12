46d74824604a312897eb9ed2a12ccc2488124395
Varshini-D17 <varshininare98@gmail.com>
2024-09-12 17:39:15 +0530

    version1 basic

---
+++ admin/location-identifier.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['sturecmsaid']==0)) {
+  header('location:logout.php');
+  } else{
+   if(isset($_POST['submit']))
+  {
+ $nottitle=$_POST['nottitle'];
+ $classid=$_POST['classid'];
+ $notmsg=$_POST['notmsg'];
+$sql="insert into tblnotice(NoticeTitle,ClassId,NoticeMsg)values(:nottitle,:classid,:notmsg)";
+$query=$dbh->prepare($sql);
+$query->bindParam(':nottitle',$nottitle,PDO::PARAM_STR);
+$query->bindParam(':classid',$classid,PDO::PARAM_STR);
+$query->bindParam(':notmsg',$notmsg,PDO::PARAM_STR);
+ $query->execute();
+   $LastInsertId=$dbh->lastInsertId();
+   if ($LastInsertId>0) {
+    echo '<script>alert("Notice has been added.")</script>';
+echo "<script>window.location.href ='add-notice.php'</script>";
+  }
+  else
+    {
+         echo '<script>alert("Something Went Wrong. Please try again")</script>';
+    }
+}
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   
+    <title>Student  Management System|| Location Report</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="vendors/select2/select2.min.css">
+    <link rel="stylesheet" href="vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="css/style.css" />
+    
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+     <?php include_once('includes/header.php');?>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+      <?php include_once('includes/sidebar.php');?>
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+              <h3 class="page-title">Location Based Student Report </h3>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                  <li class="breadcrumb-item active" aria-current="page"> Location Based Student Report</li>
+                </ol>
+              </nav>
+            </div>
+            <div class="row">
+        
+		
+    <iframe title="Student Distribution by District" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=33edca86-254a-43ff-9892-2ec86fa47207&autoAuth=true&ctid=6faddc4a-b692-41cd-aa16-efeaa97fe27c" frameborder="0" allowFullScreen="true"></iframe>
+
+             
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+         <?php include_once('includes/footer.php');?>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="vendors/select2/select2.min.js"></script>
+    <script src="vendors/typeahead.js/typeahead.bundle.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="js/off-canvas.js"></script>
+    <script src="js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="js/typeahead.js"></script>
+    <script src="js/select2.js"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html><?php }  ?>
+
